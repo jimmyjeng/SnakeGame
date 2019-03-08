9c85763e31fe1a5e98b85b85eebdffc4b40e3e6b
Rex <rex@Rex-MacBook-Pro.local>
2019-03-09 01:09:35 +0800

    init commit

---
+++ Snake/Snake/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snake
+//
+//  Created by Rex on 2019/3/9.
+//  Copyright © 2019年 Jimmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
